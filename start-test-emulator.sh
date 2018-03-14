9ca7dac301a4fefeeaa8e440ecf35e938959b293
Randy Webster <randy@randr0id.com>
2018-03-14 10:42:00 -0700

    Add support for downloading emulator on-demand. Add option to specify avd name when starting emulator.

--- start-test-emulator.sh
+++ start-test-emulator.sh
@@ -1,29 +1,30 @@
 #!/usr/bin/env bash
 
 android=26
-abi=x86
+abi='x86'
+avd_name='android_ci_test'
 
-usage="$(basename "$0") [-h] [-a android] [-b abi] -- Create and start an Android emulator.
+usage="$(basename "$0") [-h] [-a android] [-b abi] [-n name] -- Create and start an Android emulator.
 
 where:
     -h: Show this help text
     -a: Specify the emulator Android API (default: ${android})
-    -b: Specify the emulator ABI (default: ${abi})"
+    -b: Specify the emulator ABI (default: ${abi})
+    -n: Specify the emulator name (default: ${avd_name})"
 
-while getopts ':hab:' option; do
-  case "$option" in
-    h) echo "$usage"
-        exit
-        ;;
-    a) android=${OPTARG}
-        ;;
-    b) abi=${OPTARG}
-        ;;
+while getopts ':ha:b:n:' option; do
+  case "${option}" in
+    h) echo "${usage}"
+        exit ;;
+    a) android=${OPTARG} ;;
+    b) abi=${OPTARG} ;;
+    n) avd_name=${OPTARG} ;;
   esac
 done
-shift $((OPTIND - 1))
 
-echo "Starting Android emulator with API ${android} and ABI ${abi}"
-echo "no" | ${ANDROID_HOME}/tools/bin/avdmanager create avd -f -n android_test -b "google_apis/${abi}" -k "system-images;android-${android};google_apis;${abi}"
-echo "no" | ${ANDROID_HOME}/tools/emulator @android_test -wipe-data -no-audio -no-window -verbose & ${BASH_SOURCE%/*}/wait-for-emulator.sh
+echo "Starting Android emulator ${avd_name} with API ${android} and ABI ${abi}"
+${ANDROID_HOME}/tools/bin/sdkmanager "system-images;android-${android};google_apis;${abi}"
+${ANDROID_HOME}/tools/bin/avdmanager delete avd -n ${avd_name}
+echo "no" | ${ANDROID_HOME}/tools/bin/avdmanager create avd -f -n ${avd_name} -b "google_apis/${abi}" -k "system-images;android-${android};google_apis;${abi}"
+echo "no" | ${ANDROID_HOME}/tools/emulator @${avd_name} -wipe-data -no-audio -no-window -verbose & ${BASH_SOURCE%/*}/wait-for-emulator.sh
 
--- wait-for-emulator.sh
+++ wait-for-emulator.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
-OUT=`${ANDROID_HOME}/platform-tools/adb shell getprop init.svc.bootanim`
-RES="stopped"
+set +e
 
-while [[ ${OUT:0:7}  != 'stopped' ]]; do
-    OUT=`${ANDROID_HOME}/platform-tools/adb shell getprop init.svc.bootanim`
-    echo 'Waiting for emulator to fully boot...'
-    sleep 5
+bootanim=""
+counter=0
+timeout=360
+
+until [[ "${bootanim}" =~ "stopped" ]]; do
+    bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
+    if [[ "${bootanim}" =~ "device not found" || "${bootanim}" =~ "device offline" || "${bootanim}" =~ "running" ]]; then
+        let "counter += 1"
+        echo "Waiting for emulator to start..."
+
+        if [[ ${counter} -gt timeout ]]; then
+            echo "Timeout (${timeout} seconds) reached; failed to start emulator"
+            exit 1
+        fi
+    fi
+    sleep 2
 done
 
-echo "Emulator booted!"
+echo "Emulator is ready!"
 
